9bad5aa6c491a0777ea9388d14ca6aea913c75f9
Wilma Nur Fatimah <97320403+Wilmanurfatimah14@users.noreply.github.com>
2024-09-23 10:05:31 +0700

    Update Komputasi Awan pertemuan 2.py

--- Komputasi Awan pertemuan 2.py
+++ Komputasi Awan pertemuan 2.py
@@ -3,4 +3,6 @@ import streamlit as st
 x = st.number_input("Masukan Angka")
 sx = st.text_input("Satuan", "C")
 st.write("Anda memasukan", x,'',sx)
+sy = st.text_input("Dikonversi ke", "C")
+st.write (x, ' ',sx, '=', y,sy)
 

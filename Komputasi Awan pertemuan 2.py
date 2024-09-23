8a7976ddf42d5b6c9daa4ced8e242470aa1295cc
Wilma Nur Fatimah <97320403+Wilmanurfatimah14@users.noreply.github.com>
2024-09-23 10:11:10 +0700

    Update Komputasi Awan pertemuan 2.py

--- Komputasi Awan pertemuan 2.py
+++ Komputasi Awan pertemuan 2.py
@@ -4,5 +4,28 @@ x = st.number_input("Masukan Angka")
 sx = st.text_input("Satuan", "C")
 st.write("Anda memasukan", x,'',sx)
 sy = st.text_input("Dikonversi ke", "C")
+y = 0
+# Logika konversi suhu
+if sx == 'C':
+    if sy == 'F':  # Celcius ke Fahrenheit
+        y = (x * 9/5) + 32
+    elif sy == 'K':  # Celcius ke Kelvin
+        y = x + 273.15
+    elif sy == 'C':  # Jika tidak ada konversi
+        y = x
+elif sx == 'F':
+    if sy == 'C':  # Fahrenheit ke Celcius
+        y = (x - 32) * 5/9
+    elif sy == 'K':  # Fahrenheit ke Kelvin
+        y = (x - 32) * 5/9 + 273.15
+    elif sy == 'F':
+        y = x
+elif sx == 'K':
+    if sy == 'C':  # Kelvin ke Celcius
+        y = x - 273.15
+    elif sy == 'F':  # Kelvin ke Fahrenheit
+        y = (x - 273.15) * 9/5 + 32
+    elif sy == 'K':
+        y = x
 st.write (x, ' ',sx, '=', y,sy)
 
